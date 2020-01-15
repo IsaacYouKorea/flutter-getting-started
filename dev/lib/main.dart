0511081786c935deab66fabcd62afdd5c85a4471
isaac_you <isaac_you@mhand.net>
2020-01-16 00:15:53 +0900

    * Raised Button and callback function

--- dev/lib/main.dart
+++ dev/lib/main.dart
@@ -3,19 +3,44 @@ import 'package:flutter/material.dart';
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
+  var questionIndex = 0;
+  void answerQuestion(index) {
+    print(index);
+  }
+
   @override
   Widget build(BuildContext context) {
+    var questions = ["Question 1", "Question 2"];
+    var questionIndex = 0;
     return MaterialApp(
         title: 'Flutter Demo',
         theme: ThemeData(
           primarySwatch: Colors.blue,
         ),
         home: Scaffold(
-          appBar: AppBar(
-            title: Text('My First App'),
-          ),
-          body: Text('This is my default text!'),
-        ));
+            appBar: AppBar(
+              title: Text('My First App'),
+            ),
+            body: Column(
+              children: <Widget>[
+                Text(questions[questionIndex]),
+                RaisedButton(
+                  child: Text('Answer 1'),
+                  onPressed: () => answerQuestion(1),
+                ),
+                RaisedButton(
+                  child: Text('Answer 2'),
+                  onPressed: () => answerQuestion(2),
+                ),
+                RaisedButton(
+                  child: Text('Answer 3'),
+                  onPressed: () => answerQuestion(3),
+                ),
+                RaisedButton(
+                  child: Text('Answer 4'),
+                  onPressed: () => answerQuestion(4),
+                ),
+              ],
+            )));
   }
 }
